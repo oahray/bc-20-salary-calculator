95908cc48d320f19ea326746285e95fb104986c0
Oahray <rodneyoare@gmail.com>
2017-03-16 23:17:10 +0100

    modified to include input name exception handling

--- main.js
+++ main.js
@@ -5,70 +5,98 @@ var Employee = function(name, level, years) {
 
 	this.getBaseSalary = function() {
 		switch(level) {
-			case 'intern':
+			case 'Intern':
 				return 40000;
-			case 'associate':
+			case 'Associate':
 				return 60000;
-			case 'manager':
+			case 'Manager':
 				return 80000;
-			case 'executive':
+			case 'Executive':
 				return 100000;
-			case 'director':
+			case 'Director':
 				return 120000;
 		}
 	};
 
 	this.getBenefits = function() {
 		this.baseSalary = this.getBaseSalary();
-		if (this.years < 3) {
-			return 0;
-		}
-		else if (this.years < 5) {
-			return this.baseSalary * 0.2;
-		}
-		else if (this.years < 10) {
-			return this.baseSalary * 0.3;
-		}
-		else {
-			return this.baseSalary * 0.5;
+		switch(years) {
+			case('0-2'):
+				return 0;
+			case('3-4'):
+				return this.baseSalary * 0.2;
+			case('5-6'):
+				return this.baseSalary * 0.4;
+			case('7-8'):
+				return this.baseSalary * 0.6;
+			case('above 8'):
+				return this.baseSalary * 0.8;
 		}
 	};
 
 	this.salary = this.getBaseSalary() + this.getBenefits();
 
 	this.getSalaryReport = function() {
-	  return this.name + " earns " + this.salary.toString() + " monthly. This includes a Basic Salary of N" + this.getBaseSalary().toString() + " and N" + this.getBenefits().toString() + " as benefits.";
+	  return this.name + " earns N" + this.salary.toString() + " monthly. This includes a Basic Salary of N" + this.getBaseSalary().toString() + " and N" + this.getBenefits().toString() + " as benefits.";
 	};
 	
 	this.salaryReport = this.getSalaryReport();
 };
 
-var ray= new Employee("Ray", "manager", 7);
-console.log(ray.salaryReport);
+// var ray= new Employee("Ray", "manager", 7);
+// console.log(ray.salaryReport);
 
 
 var button= document.getElementById("submit");
 
-function displayDetails(){
-    var names =document.getElementById("name").value,
-	years=Number(document.getElementById("years").value),
-	level=document.getElementById("cadreSelection").value;    
-    console.log("Clicked");
-    console.log(names, years, level);
-    if (!names || !level || !years){
-        console.log( "you missed filling a detail or some details");
-    }else if (typeof name !=="string"){        
-        console.log( "name should be atleast 3 characters");          
-    }else if (typeof years !=="number"){
-        console.log("Years should be number")
-    }else{
-        document.getElementById("report-name").innerHTML= "Name: " + names;
-        document.getElementById("report-cadre").innerHTML="Level: " + level;
-        document.getElementById("report-years").innerHTML= "Years Of Experience: " + years;
-        var sal= new Employee(name, level, years);
-        document.getElementById("report-summary").innerHTML= "Salary summary: " + sal.salaryReport;
-        document.getElementById("report").style.display="block";
+function validate(name){
+	var clean = true;
+  for (var i = 0; i < name.length; i++) {
+    if (!Boolean(name[i].match(/[a-zA-Z]|\s|-/g))) {
+      clean = false;
     }
+	};
+	return clean;
+};
+
+var properName = function(name) {
+	name = name.split(" ");
+	proper = [];
+	for (var i = 0; i < name.length; i++) {
+	  var eachName = [];
+	  for (var j = 0; j < name[i].length; j++) {
+	    if (j === 0) {
+	      eachName.push(name[i][0].toUpperCase());
+	    }
+	    else {
+	      eachName.push(name[i][j].toLowerCase());
+	    }
+	  }
+	  proper.push(eachName.join(""));
+	}
+	return proper.join(" ");
+};
+
+function displayDetails(){
+  var name = document.getElementById("name").value, 
+  years = document.getElementById("years").value,
+  level = document.getElementById("cadreSelection").value;    
+  // console.log("Clicked");
+  // console.log(names, years, level);
+  if (!name || !level || !years){
+      alert( "you missed filling a detail or some details");
+  }else if (!validate(name)){        
+      alert( "Please input a proper name");          
+  }else{
+  	name = properName(name);
+    document.getElementById("report-name").innerHTML= "Name: " + name;
+    document.getElementById("report-cadre").innerHTML="Level: " + level;
+    document.getElementById("report-years").innerHTML= "Years Of Experience: " + years + " years";
+    var person = new Employee(name, level, years);
+    document.getElementById("report-summary").innerHTML= "Salary summary: " + person.salaryReport;
+    console.log(name);
+    document.getElementById("report").style.display="block";
+  }
 }
 
 button.addEventListener("click", displayDetails, false);
